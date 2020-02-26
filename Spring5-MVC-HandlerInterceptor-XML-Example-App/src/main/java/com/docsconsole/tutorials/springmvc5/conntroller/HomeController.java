8790ab020a2ae751c1c5aafc0498677c698b7e2e
amarsivas <amarsivas.b@gmail.com>
2020-02-26 19:59:42 +0530

    Spring5-MVC-HandlerInterceptor-XML-Example-App
    Demo Example Application for HandlerInterceptor using XML Configuration

---
+++ Spring5-MVC-HandlerInterceptor-XML-Example-App/src/main/java/com/docsconsole/tutorials/springmvc5/conntroller/HomeController.java
@@ -0,0 +1,33 @@
+package com.docsconsole.tutorials.springmvc5.conntroller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.docsconsole.tutorials.springmvc5.interceptor.AdminRequestInterceptor;
+
+@Controller
+public class HomeController {
+	private static final Logger logger = Logger.getLogger(HomeController.class);
+	@RequestMapping(value="/getAdminHome", method = RequestMethod.GET)
+	public String getAdminHome(ModelMap model) {
+		
+		logger.info("Request @getAdminHome-HomeController");
+		model.addAttribute("msg", "Welcome Admin to AdminHome Page !!");
+		logger.info("Sending response @getAdminHome-HomeController");
+		return "adminHome";		
+		
+	}
+	
+	@RequestMapping(value="/getUserHome", method = RequestMethod.GET)
+	public String getUserHome(ModelMap model) {
+		
+		logger.info("Request @getUserHome-HomeController");
+		model.addAttribute("msg", "Welcome User to UserHome Page !!");
+		logger.info("Sending response @getUserHome-HomeController");
+		return "userHome";		
+		
+	}
+}
---
+++ Spring5-MVC-HandlerInterceptor-XML-Example-App/src/main/java/com/docsconsole/tutorials/springmvc5/interceptor/UserRequestInterceptor.java
@@ -0,0 +1,35 @@
+package com.docsconsole.tutorials.springmvc5.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class UserRequestInterceptor extends HandlerInterceptorAdapter {
+	private static final Logger logger = Logger.getLogger(UserRequestInterceptor.class);
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		logger.info("Request intercepted @preHandle-UserRequestInterceptor");
+		logger.info("Request URL::" + request.getRequestURL().toString());
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		logger.info("Request intercepted @postHandle-UserRequestInterceptor");
+		logger.info("Request URL::" + request.getRequestURL().toString());
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		logger.info("Request intercepted @afterCompletion-UserRequestInterceptor");
+		logger.info("Request URL::" + request.getRequestURL().toString());
+	}
+}
